d3b3966c15ef7922499685aca65f089156d1e718
Yves Candau <ycandau@gmail.com>
2021-07-04 04:09:44 +0000

    Added read eval loop for user input

--- client.js
+++ client.js
@@ -1,10 +1,48 @@
+//------------------------------------------------------------------------------
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const ask = (client) => {
+  rl.question('> ', (answer) => {
+    switch (answer) {
+      case 'ls':
+        console.log('ls');
+        ask(client);
+        break;
+      case 'dl':
+        console.log('dl');
+        ask(client);
+        break;
+      case 'end':
+        console.log('end');
+        rl.close();
+        client.end();
+        break;
+      default:
+        console.log('Invalid command');
+        ask(client);
+        break;
+    }
+  });
+};
+
+//------------------------------------------------------------------------------
+
 const net = require('net');
 
 const [port, hostname] = [3000, 'localhost'];
 
 const client = new net.Socket();
 
-client.connect(port, hostname, () => console.log('Connected to server'));
+client.connect(port, hostname, () => {
+  console.log('Connected to server');
+  ask(client);
+});
 
 client.on('data', (data) => console.log(`From server: ${data}`));
 
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = net.createServer((socket) => {
   console.log('Client connected');
   socket.on('end', () => console.log('Client disconnected'));
   socket.on('error', (error) => console.log(error.message));
-  socket.write('Testing write');
+  // socket.write('Testing write');
 });
 
 server.listen(port, hostname);
